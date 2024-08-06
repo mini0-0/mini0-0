8133d9ca653ea17140233963223d46c85c98c999
mini0-0 <nalala8200@naver.com>
2024-08-06 11:21:14 +0900

    mini0-0 update

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -5,20 +5,73 @@ import Parser from "rss-parser";
  * README.MD에 작성될 페이지 텍스트
  * @type {string}
  */
-let text = `# This is Mini's GitHub👋
+let text = `<h1 align="center"> This is Mini's GitHub👋 </h1>
+# This is Mini's GitHub👋
 
+<h2 align="center"> 🖥️ Tech Stacks 🖥️ </h2>
+<h3 align="center">Tools</h3>
+ <div align="center">
+  <img src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white">
+  <img src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=java&logoColor=white"> 
+
+ </div>
+
+ <div align="center">
+   <img src="https://img.shields.io/badge/Django-092E20?style=flat-square&logo=django&logoColor=white">
+   <img src="https://img.shields.io/badge/MySql-4479A1?style=fflat-square&logo=mysql&logoColor=white"> 
+   <img src="https://img.shields.io/badge/Sqlite-003B57?style=fflat-square&logo=sqlite&logoColor=white"> 
+   <img src="https://img.shields.io/badge/AmazonEC2-FF9900.svg?style=flat-square&logo=amazonec2&logoColor=white"/>
+ </div>
+
+ <div align="center">
+   <img src="https://img.shields.io/badge/Jupyter-F37626.svg?style=flat-square&logo=jupyter&logoColor=white"/>
+  <img src="https://img.shields.io/badge/GoogleColab-F9AB00.svg?style=flat-square&logo=googlecolabr&logoColor=white"/>
+   <img src="https://img.shields.io/badge/Intellijidea-000000.svg?style=flat-square&logo=intellijidea&logoColor=white"/>
+  <img src="https://img.shields.io/badge/PyCharm-000000.svg?style=flat-square&logo=pycharm&logoColor=white"/>
+ </div>
+
+ <div align="center">
+   <img src="https://img.shields.io/badge/pandas-150458.svg?style=flat-square&logo=pandas&logoColor=white"/>
+   <img src="https://img.shields.io/badge/numpy-013243.svg?style=flat-square&logo=numpy&logoColor=white"/>
+   <img src="https://img.shields.io/badge/scikitlearn-F7931E.svg?style=flat-square&logo=scikitlearn&logoColor=white"/>
+   <img src="https://img.shields.io/badge/tensorflow-FF6F00.svg?style=flat-square&logo=tensorflow&logoColor=white"/>
+  <img src="https://img.shields.io/badge/pytorch-EE4C2C.svg?style=flat-square&logo=pytorch&logoColor=white"/>
+ </div>
 [![Solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=nalala8200)](https://solved.ac/nalala8200)
 
+<h3 align="center">Team Tools</h3>
+<div align="center">
+ <img src="https://img.shields.io/badge/git-%23F05033.svg?style=flat-square&logo=git&logoColor=white"/> 
+ <img src="https://img.shields.io/badge/github-%23121011.svg?style=flat-square&logo=github&logoColor=white"/>  
+ <img src="https://img.shields.io/badge/Slack-4A154B?style=flat-square&logo=slack&logoColor=white"/> 
+ <img src="https://img.shields.io/badge/Notion-%23000000.svg?style=flat-square&logo=notion&logoColor=white"/>
  <a href="https://github.com/anuraghazra/github-readme-stats">
      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mini0-0&layout=donut&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true&exclude_repo=Face-Transfer-Application" width=38% />
  </a>    
- 
+
+ </div>
+<br>
+
+<h2 align="center">  📝GitHub Status</h2>
+<div align="center">
+
+ [![Solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=nalala8200)](https://solved.ac/nalala8200)
+
+  <a href="https://github.com/anuraghazra/github-readme-stats">
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mini0-0&layout=donut&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true&exclude_repo=Face-Transfer-Application" width=38% />
+  </a>    
+
+  <a href="https://github.com/anuraghazra/github-readme-stats">
+    <img src="https://github-readme-stats.vercel.app/api?username=mini0-0&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true" width=56% />
+  </a>
+</div>
  <a href="https://github.com/anuraghazra/github-readme-stats">
    <img src="https://github-readme-stats.vercel.app/api?username=mini0-0&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true" width=56% />
  </a>
 
 
-## 📕 Latest Blog Posts
+<h2 align="center">  📕 Latest Blog Posts </h2>
+<div align="center">
 
 `;
 
