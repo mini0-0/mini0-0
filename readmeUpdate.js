e78984dc8579c001aa1097e1ece5122047affcc2
mini <nalala8200@naver.com>
2024-09-12 17:21:09 +0900

    Update readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -52,11 +52,9 @@ let text = `<h1 align="center"> This is Mini's GitHub👋 </h1>
  [![Solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=nalala8200)](https://solved.ac/nalala8200)
 
   <a href="https://github.com/anuraghazra/github-readme-stats">
-      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mini0-0&layout=donut&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true&exclude_repo=Face-Transfer-Application" width=100% />
-  </a>    
-
-  <a href="https://github.com/anuraghazra/github-readme-stats">
-    <img src="https://github-readme-stats.vercel.app/api?username=mini0-0&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true" width=100% />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mini0-0&layout=donut&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true&exclude_repo=Face-Transfer-Application" width=50% />
+  </a> <a href="https://github.com/anuraghazra/github-readme-stats">
+    <img src="https://github-readme-stats.vercel.app/api?username=mini0-0&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true" width=50% />
   </a>
 </div>
 
